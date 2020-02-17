f435df681d89d39f48074e07df984fce6dbfee75
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-02-17 13:36:36 -0800

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,5 +1,43 @@
 
+def addition(n1, n2)
+n1 = 5
+n2 = 4
+sum = n1 + n2
+sum
+end
+
+def subtraction(n1, n2)
+  n1 = 10
+  n2 = 5
+  sum = n1 - n2
+  sum
+end
+
+def division(n1, n2)
+  n1 = 50
+  n2 = 2
+  sum = n1 / n2
+  sum
+end
+
+def multiplication(n1, n2)
+  n1 = 4
+  n2 = 30
+  sum = n1 * n2
+  sum
+end
   
+def modulo(n1, n2)
+  n1 = 34
+  n2 = 5
+  sum = 34 % 5
+  sum
+end
+
+def square_root(x)
+  x = Math.sqrt(81)
+  x
+end
   
   
   
